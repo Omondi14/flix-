2a6b4eef911a9f91edaa89293d1d68f3e1fbef38
Ernest Omondi <eomondi@fb.com>
2018-06-27 11:08:22 -0700

    Initial Commit

---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Ernest Omondi on 6/27/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flix/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Flix
+//
+//  Created by Ernest Omondi on 6/27/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
