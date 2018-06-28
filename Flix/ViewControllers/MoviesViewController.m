a022760653915fd89a05881f8957804a74f2e161
Ernest Omondi <eomondi@fb.com>
2018-06-28 15:08:33 -0700

    user story 3 done

--- Flix/ViewControllers/MoviesViewController.m
+++ Flix/ViewControllers/MoviesViewController.m
@@ -13,8 +13,8 @@
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>  
 
 @property (nonatomic,strong)NSArray *movies;
-
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
 
@@ -26,6 +26,17 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    
+    [self fetchMovies];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+//    [self.tableView addSubview:_refreshControl];
+    
+}
+
+-  (void)fetchMovies {
     
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -49,10 +60,10 @@
             }
             
             [self.tableView reloadData];
-            // TODO: Get the array of movies
-            // TODO: Store the movies in a property to use elsewhere
-            // TODO: Reload your table view data
         }
+        
+        [self.refreshControl endRefreshing];
+        
     }];
     [task resume];
     
