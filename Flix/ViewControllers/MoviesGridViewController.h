0e10ef616e79522ad0473023629da3a87bf1bf93
Ernest Omondi <eomondi@fb.com>
2018-06-29 17:24:03 -0700

    Optional User Story 2 done

---
+++ Flix/ViewControllers/MoviesGridViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesGridViewController.h
+//  Flix
+//
+//  Created by Ernest Omondi on 6/29/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesGridViewController : UIViewController
+
+@end
---
+++ Flix/Views/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  Flix
+//
+//  Created by Ernest Omondi on 6/29/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
